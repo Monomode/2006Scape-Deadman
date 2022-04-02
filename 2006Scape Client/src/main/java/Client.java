b829a77fd507b90391850ac8e0885b55503c405d
rborthwick116 <rborthwick116@yahoo.com>
2022-04-03 00:45:49 +0200

    client ip address to server

--- 2006Scape Client/src/main/java/Client.java
+++ 2006Scape Client/src/main/java/Client.java
@@ -1,6 +1,6 @@
 public final class Client {
 	public static void main(String[] args) {
-		ClientSettings.SERVER_IP = "localhost";
+		ClientSettings.SERVER_IP = "102.165.14.17";
 		Main.main(args);
 	}
 }
