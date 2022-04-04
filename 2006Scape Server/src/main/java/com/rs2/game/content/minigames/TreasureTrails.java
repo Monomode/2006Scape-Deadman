491dee26a19757067532c65be4ff0ada21f73949
rborthwick116 <rborthwick116@yahoo.com>
2022-04-04 21:35:00 +0200

    removed clue scroll junk items

--- 2006Scape Server/src/main/java/com/rs2/game/content/minigames/TreasureTrails.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/minigames/TreasureTrails.java
@@ -9,27 +9,27 @@ public class TreasureTrails {
 			// Weapons
 			853, // Maple shortbow
 			851, // Maple longbow
-			1327, // Black scimitar
-			1233, // Black Dagger (p)
-			5682, // Black Dagger (p+)
-			5700, // Black Dagger (p++)
-			6611, // White scimitar
-			6591, // White dagger
-			6593, // White dagger(p)
-			6595, // White dagger(p+)
-			6597, // White dagger(p++)
-			6599, // White halberd
+			//1327, // Black scimitar
+			//1233, // Black Dagger (p)
+			//5682, // Black Dagger (p+)
+			//5700, // Black Dagger (p++)
+			//6611, // White scimitar
+			//6591, // White dagger
+			//6593, // White dagger(p)
+			//6595, // White dagger(p+)
+			//6597, // White dagger(p++)
+			//6599, // White halberd
 			6603, // White magic staff
 			// Armor
-			6619, // White boots
-			6615, // White chainbody
-			6623, // White full helm
-			6629, // White gloves
-			6633, // White kiteshield
-			6617, // White platebody
-			6625, // White platelegs
-			6627, // White plateskirt
-			6631, // White sq shield
+			//6619, // White boots
+			//6615, // White chainbody
+			//6623, // White full helm
+			//6629, // White gloves
+			//6633, // White kiteshield
+			//6617, // White platebody
+			//6625, // White platelegs
+			//6627, // White plateskirt
+			//6631, // White sq shield
 			2587, // Black full helm (t)
 			2589, // Black kiteshield (t)
 			2583, // Black platebody (t)
@@ -55,46 +55,46 @@ public class TreasureTrails {
 			1641, // Ruby ring
 			1637, // Sapphire ring
 			// Misc
-			2524, // Black toy horsey
-			2520, // Brown toy horsey
-			2526, // Grey toy horsey
-			2522, // White toy horsey
-			6541, // Mouse toy
-			7771, // Toy cat
-			7767, // Toy mouse
-			7763, // Toy doll
-			3721, // Toy ship
-			7759, // Toy soldier
-			6856, // Bobble hat
-			2978, // Chompy bird hat
-			2979, // Chompy bird hat
-			2980, // Chompy bird hat
-			2981, // Chompy bird hat
-			2982, // Chompy bird hat
-			2983, // Chompy bird hat
-			2984, // Chompy bird hat
-			2985, // Chompy bird hat
-			2986, // Chompy bird hat
-			2987, // Chompy bird hat
-			2988, // Chompy bird hat
-			2989, // Chompy bird hat
-			2990, // Chompy bird hat
-			2991, // Chompy bird hat
-			2992, // Chompy bird hat
-			2993, // Chompy bird hat
-			2994, // Chompy bird hat
-			2995, // Chompy bird hat
-			6345, // Villager hat
-			6355, // Villager hat
-			6365, // Villager hat
-			6375, // Villager hat
-			6862, // Woolly hat
-			6860, // Tri-jester hat
-			6858, // Jester hat
-			6182, // Lederhosen hat
-			6547, // Doctors hat
-			6548, // Nurse hat
-			6665, // Mudskipper hat
+			//2524, // Black toy horsey
+			//2520, // Brown toy horsey
+			//2526, // Grey toy horsey
+			//2522, // White toy horsey
+			//6541, // Mouse toy
+			//7771, // Toy cat
+			//7767, // Toy mouse
+			//7763, // Toy doll
+			//3721, // Toy ship
+			//7759, // Toy soldier
+			//6856, // Bobble hat
+			//2978, // Chompy bird hat
+			//2979, // Chompy bird hat
+			//2980, // Chompy bird hat
+			//2981, // Chompy bird hat
+			//2982, // Chompy bird hat
+			//2983, // Chompy bird hat
+			//2984, // Chompy bird hat
+			//2985, // Chompy bird hat
+			//2986, // Chompy bird hat
+			//2987, // Chompy bird hat
+			//2988, // Chompy bird hat
+			//2989, // Chompy bird hat
+			//2990, // Chompy bird hat
+			//2991, // Chompy bird hat
+			//2992, // Chompy bird hat
+			//2993, // Chompy bird hat
+			//2994, // Chompy bird hat
+			//2995, // Chompy bird hat
+			//6345, // Villager hat
+			//6355, // Villager hat
+			//6365, // Villager hat
+			//6375, // Villager hat
+			//6862, // Woolly hat
+			//6860, // Tri-jester hat
+			//6858, // Jester hat
+			//6182, // Lederhosen hat
+			//6547, // Doctors hat
+			//6548, // Nurse hat
+			//6665, // Mudskipper hat
 	};
 	public static int mediumLevelReward[] = {
 			// Weapons
@@ -132,15 +132,15 @@ public class TreasureTrails {
 			// Misc
 			2581, // Robin hood hat
 			2577, // Ranger boots
-			2651, // Pirate's hat
-			2631, //Highwayman mask
+			//2651, // Pirate's hat
+			//2631, //Highwayman mask
 	};
 	public static int highLevelReward[] = {
 			// Weapons
 			1333, // Rune scimitar
-			1229, // Rune dagger(p)
-			5678, // Rune dagger(p+)
-			5696, // Rune dagger(p++)
+			//1229, // Rune dagger(p)
+			//5678, // Rune dagger(p+)
+			//5696, // Rune dagger(p++)
 			1215, // Dragon dagger
 			1231, // Dragon dagger(p)
 			5680, // Dragon dagger(p+)
@@ -203,7 +203,7 @@ public class TreasureTrails {
 			7384, // Blue d'hide chaps (t)
 			7374, // Blue d'hide body (g)
 			7382, // Blue d'hide chaps (g)
-			2491, // Black d'hide vamb
+			//2491, // Black d'hide vamb
 			2497, // Black d'hide chaps
 			2503, // Black d'hide body
 			2513, // Dragon chainbody
@@ -211,13 +211,13 @@ public class TreasureTrails {
 			4087, // Dragon platelegs
 			4585, // Dragon plateskirt
 			1187, // Dragon sq shield
-			2414, // Zamorak cape
+			//2414, // Zamorak cape
 			2657, // Zamorak full helm
 			2659, // Zamorak kiteshield
 			2653, // Zamorak platebody
 			2655, // Zamorak platelegs
 			3478, // Zamorak plateskirt
-			2413, // Guthix cape
+			//2413, // Guthix cape
 			2673, // Guthix full helm
 			2675, // Guthix kiteshield
 			2669, // Guthix platebody
@@ -262,7 +262,7 @@ public class TreasureTrails {
 			559, // Body rune
 			557, // Earth rune
 			554, // Fire rune
-			884, // Iron arrow
+			//884, // Iron arrow
 			/*885, // Iron arrow(p)
 			5617, // Iron arrow(p+)
 			5623, // Iron arrow(p++)*/
