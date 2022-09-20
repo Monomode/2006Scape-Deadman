083e3904bd232d795d86b75f0b2f4d6735136b10
rborthwick116 <rborthwick116@yahoo.com>
2022-09-20 23:58:22 +0200

    Desert Treasure quest

--- 2006Scape Server/src/main/java/com/rs2/game/content/quests/impl/DesertTreasure.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/quests/impl/DesertTreasure.java
@@ -9,7 +9,6 @@ import com.rs2.game.players.Player;
 
 public class DesertTreasure {
 
-
 	public static void showInformation(Player client) {
 		for (int i = 8144; i < 8295; i++) {
 			client.getPacketSender().sendString("", i);
@@ -17,26 +16,31 @@ public class DesertTreasure {
 		client.getPacketSender().sendString("@dre@Desert Treasure", 8144);
 		client.getPacketSender().sendString("", 8145);
 		if (client.desertT == 0) {
-			client.getPacketSender().sendString( "I can start this quest by speaking to Wizard Mizgog who is", 8147);
-			client.getPacketSender().sendString("in the Wizard's Tower.", 8148);
+			client.getPacketSender().sendString( "I can start this quest by speaking to an Archaeologist", 8147);
+			client.getPacketSender().sendString("who is at the Bedabin Camp in Al-Kharid Desert.", 8148);
+			client.getPacketSender().sendString("", 8149);
+			client.getPacketSender().sendString("Quest Requirements:", 8150);
+			client.getPacketSender().sendString("1000 Total Level", 8151);
+			client.getPacketSender().sendString("50 Firemaking Level", 8152);
+			client.getPacketSender().sendString("53 Thieving Level", 8153);
 		} else if (client.desertT == 1) {
-			client.getPacketSender().sendString("@str@I can start this quest by speaking to Wizard Mizgog who is", 8147);
-			client.getPacketSender().sendString("@str@in the Wizard's Tower.", 8148);
+			client.getPacketSender().sendString("@str@I can start this quest by speaking to an Archaeologist", 8147);
+			client.getPacketSender().sendString("@str@who is at the Bedabin Camp in Al-Kharid Desert.", 8148);
 			client.getPacketSender().sendString("", 8149);
-			client.getPacketSender().sendString("Wizard Mizgog have asked you to get the following items:", 8150);
-			client.getPacketSender().sendString("Red bead", 8151);
-			client.getPacketSender().sendString("Yellow bead", 8152);
-			client.getPacketSender().sendString("Black bead", 8153);
-			client.getPacketSender().sendString("White bead", 8154);
+			client.getPacketSender().sendString("The Archaeologist has asked you to get the following items:", 8150);
+			client.getPacketSender().sendString("@red@Blood diamond", 8151);
+			client.getPacketSender().sendString("@yel@Shadow diamond", 8152);
+			client.getPacketSender().sendString("Smoke diamond", 8153);
+			client.getPacketSender().sendString("@whi@Ice diamond", 8154);
 		} else if (client.desertT == 2) {
-			client.getPacketSender().sendString("@str@I can start this quest by speaking to Wizard Mizgog who is", 8147);
-			client.getPacketSender().sendString("@str@in the Wizard's Tower.", 8148);
+			client.getPacketSender().sendString("@str@I can start this quest by speaking to an Archaeologist", 8147);
+			client.getPacketSender().sendString("@str@who is at the Bedabin Camp in Al-Kharid Desert.", 8148);
 			client.getPacketSender().sendString("", 8149);
-			client.getPacketSender().sendString("@str@Wizard Mizgog have asked you to get the following items:", 8150);
-			client.getPacketSender().sendString("@str@Red bead", 8151);
-			client.getPacketSender().sendString("@str@Yellow bead", 8152);
-			client.getPacketSender().sendString("@str@Black bead", 8153);
-			client.getPacketSender().sendString("@str@White bead", 8154);
+			client.getPacketSender().sendString("@str@The Archaeologist has asked you to get the following items:", 8150);
+			client.getPacketSender().sendString("@str@Blood diamond", 8151);
+			client.getPacketSender().sendString("@str@Shadow diamond", 8152);
+			client.getPacketSender().sendString("@str@Smoke diamond", 8153);
+			client.getPacketSender().sendString("@str@Ice diamond", 8154);
 			client.getPacketSender().sendString("", 8155);
 			client.getPacketSender().sendString("You have completed this quest!", 8156);
 		}
