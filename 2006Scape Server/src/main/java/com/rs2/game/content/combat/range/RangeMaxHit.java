816aad84fadc76e13115516fd989b03b4229d54b
rborthwick116 <rborthwick116@yahoo.com>
2022-09-21 21:56:35 +0200

    patched ranged strength for crystal bow

--- 2006Scape Server/src/main/java/com/rs2/game/content/combat/range/RangeMaxHit.java
+++ 2006Scape Server/src/main/java/com/rs2/game/content/combat/range/RangeMaxHit.java
@@ -1,10 +1,12 @@
 package com.rs2.game.content.combat.range;
 
 import com.rs2.GameConstants;
+import com.rs2.game.npcs.NpcHandler;
 import com.rs2.game.players.Player;
+import com.rs2.util.Misc;
 
 public class RangeMaxHit {
-	
+
 	public static int calculateRangeDefence(Player c) {
 		int defenceLevel = c.playerLevel[GameConstants.DEFENCE];
 		if (c.getPrayer().prayerActive[0]) {
@@ -78,7 +80,7 @@ public class RangeMaxHit {
 	}
 
 	public static int getRangeStr(int i) {
-		int str = 0;
+		int str = 87; // 78 for crystal bow, 0 for default, 106 for bofa
 		int[][] data = { { 877, 49 }, { 881, 100 }, { 9140, 46 }, { 9145, 36 }, { 9141, 64 },
 				{ 9142, 82 }, { 9143, 100 }, { 9144, 115 }, { 9236, 14 },
 				{ 9237, 30 }, { 9238, 48 }, { 9239, 66 }, { 9240, 83 },
@@ -91,9 +93,9 @@ public class RangeMaxHit {
 				{ 825, 6 }, { 826, 10 }, { 827, 12 }, { 828, 18 }, { 829, 28 },
 				{ 830, 42 }, { 800, 5 }, { 801, 7 }, { 802, 11 }, { 803, 16 },
 				{ 804, 23 }, { 805, 36 }, { 9976, 0 }, { 9977, 15 },
-				{ 4212, 70 }, { 4214, 70 }, { 4215, 70 }, { 4216, 70 },
-				{ 4217, 70 }, { 4218, 70 }, { 4219, 70 }, { 4220, 70 },
-				{ 4221, 70 }, { 4222, 70 }, { 4223, 70 }, { 6522, 49 },
+				{ 4212, 78 }, { 4214, 78 }, { 4215, 78 }, { 4216, 78 },
+				{ 4217, 78 }, { 4218, 78 }, { 4219, 78 }, { 4220, 78 },
+				{ 4221, 78 }, { 4222, 78 }, { 4223, 78 }, { 6522, 49 },
 				{ 10034, 15 }, };
 		for (int[] element : data) {
 			if (i == element[0]) {
